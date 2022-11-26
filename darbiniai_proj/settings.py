94214bb86ac27cd8ad3c67479680ec7b0f732bba
PurpJell <115624117+PurpJell@users.noreply.github.com>
2022-11-26 20:51:28 +0200

    Set DEBUG based on environment variables

--- darbiniai_proj/settings.py
+++ darbiniai_proj/settings.py
@@ -158,5 +158,11 @@ LOGIN_URL = 'users:login'
 
 
 # Heroku settings
+import os
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == "FALSE":
+    DEBUG = False
