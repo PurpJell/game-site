c5a399fd76f05e92895a2074914ae59f36307622
PurpJell <115624117+PurpJell@users.noreply.github.com>
2022-11-30 22:27:53 +0200

    Migrations folder

--- darbiniai_proj/settings.py
+++ darbiniai_proj/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
