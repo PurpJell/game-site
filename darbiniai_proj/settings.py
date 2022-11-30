0ec2f4469e1568e8bfd1570afafa64f14d3751df
PurpJell <115624117+PurpJell@users.noreply.github.com>
2022-11-30 21:57:51 +0200

    Debugging

--- darbiniai_proj/settings.py
+++ darbiniai_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e#0b+s-b^c5s)*+rzkyym$!sp(imcl6co4aezzia=eh-=$*$*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -64,6 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
