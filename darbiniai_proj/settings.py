836405404e34f05b1f2e5e7d900380cc0c95c91d
PurpJell <115624117+PurpJell@users.noreply.github.com>
2022-12-19 12:09:32 +0200

    DEBUG = FALSE

--- darbiniai_proj/settings.py
+++ darbiniai_proj/settings.py
@@ -165,5 +165,5 @@ django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-# elif os.environ.get('DEBUG') == "FALSE":
-#     DEBUG = False
+elif os.environ.get('DEBUG') == "FALSE":
+    DEBUG = False
